07bf5b72f4f6bab3595a48f680a7a9371f4b3cf4
Sourabh Aswal <sourabhaswal98@gmail.com>
2023-04-05 13:06:16 +0530

    Alll code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NETFRAMWORKPROJECT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
